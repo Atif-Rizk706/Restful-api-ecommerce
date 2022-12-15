6e51ef3638c28e24deb1d854dabb89ef423b8729
Atif-Rizk706 <atifrizk88@gmail.com>
2022-12-15 14:50:20 +0200

    created mpdel -a

---
+++ app/Http/Controllers/RewiersController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rewiers;
+use App\Http\Requests\StoreRewiersRequest;
+use App\Http\Requests\UpdateRewiersRequest;
+
+class RewiersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreRewiersRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRewiersRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Rewiers  $rewiers
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Rewiers $rewiers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Rewiers  $rewiers
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Rewiers $rewiers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateRewiersRequest  $request
+     * @param  \App\Models\Rewiers  $rewiers
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRewiersRequest $request, Rewiers $rewiers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Rewiers  $rewiers
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Rewiers $rewiers)
+    {
+        //
+    }
+}
