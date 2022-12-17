c46cb3e7f7d50a8ef0db23171268f3a0c4e2012d
Atif-Rizk706 <atifrizk88@gmail.com>
2022-12-17 12:33:59 +0200

    migrartion created for product and review

--- database/migrations/2022_12_15_100009_create_products_table.php
+++ database/migrations/2022_12_15_100009_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class  CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,12 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->text('detales');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
